34ab9bcfba6d6e59c117e746964ff0128e825192
Juan Duarte <juan.duarte.a@gmail.com>
2023-08-22 22:54:54 -0500

    Globant riddle 2 now save results in file

--- src/Globant20Years/Riddle2.java
+++ src/Globant20Years/Riddle2.java
@@ -1,6 +1,6 @@
 package Globant20Years;
 
-import java.util.ArrayList;
+import java.io.*;
 
 /*
 Riddle #2
@@ -17,33 +17,41 @@ spaces, letters, symbols, etc.
  */
 public class Riddle2 {
 
-    private long result = -1;
-    private final int THREADS = 8;
+    private final long THREADS = 8L;
+    private final File resultsFile = new File("/home/juan/IdeaProjects/CodeChallenges/src/" +
+            "Globant20Years/results/results.txt");
 
     public static void main(String[] args) {
-        System.out.println("Globant 20 years. Riddle 2.");
-//        System.out.println(Long.MAX_VALUE);
+        System.out.println("Globant 20 years. Riddle 2 solution.");
+        System.out.println(Long.MAX_VALUE);
         Riddle2 riddle = new Riddle2();
         riddle.start();
     }
 
     public void start() {
-        long start = 1011235955046179L;
+        long start = 19L;
         long step = 10_000_000_000L;
-//        ArrayList<RiddleThread> threads = new ArrayList<>();
-//
-//        for (int i = 0; i < THREADS; i++) {
-//            RiddleThread riddleThread = new RiddleThread(start + (step * i), step, i);
-//            threads.add(riddleThread);
-//            new Thread(riddleThread).start();
-//        }
-        long l1 = 1011235955056179779L;
-        long digits = String.valueOf(l1).length();
-        long lastDigit = (l1 - (10L * (l1 / 10L)));
-        long l2 = (l1 / 10L) + lastDigit * RiddleThread.powerOfTen(digits - 1L);
-        long l3 = l2 - l1*9;
-        System.out.println(l3);
-        System.out.println(String.valueOf(l3).length());
+
+        for (long i = 0L; i < THREADS; i++) {
+            RiddleThread riddleThread = new RiddleThread(start + (step * i), step, i);
+            new Thread(riddleThread).start();
+        }
+//        long l1 = 1011235955056179779L;
+//        long digits = String.valueOf(l1).length();
+//        long lastDigit = (l1 - (10L * (l1 / 10L)));
+//        long l2 = (l1 / 10L) + lastDigit * RiddleThread.powerOfTen(digits - 1L);
+//        long l3 = l2 - l1*9;
+//        System.out.println(l3);
+//        System.out.println(String.valueOf(l3).length());
+    }
+
+    public void saveResult(String result) {
+        try (FileWriter fileWriter = new FileWriter(resultsFile, true);
+             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter)) {
+            bufferedWriter.write(String.format(result + "%n"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     class RiddleThread implements Runnable {
@@ -51,13 +59,13 @@ public class Riddle2 {
         private final long start;
         private final long step;
         private long actualStep;
-        private final int id;
+        private final long id;
 
-        public RiddleThread(long start, long step, int id) {
+        public RiddleThread(long start, long step, long id) {
             this.id = id;
             this.start = start;
             this.step = step;
-            this.actualStep = 0;
+            this.actualStep = 0L;
         }
 
         @Override
@@ -66,28 +74,36 @@ public class Riddle2 {
             long firstDigit;
             long lastDigit;
             long value;
+            long factor = 9L;
+            long difference;
 
-            while (actualStep < 10000) {
+            while (true) {
                 long num = start + (step * THREADS * actualStep);
                 System.out.println("Thread " + id + ": " + num);
 
-                for ( ;num < start + (step * THREADS * actualStep) + step; num += 10) {
+                for ( ;num < start + (step * THREADS * actualStep) + step; num += 10L) {
                     digits = String.valueOf(num).length();
                     firstDigit = num / powerOfTen(digits - 1L);
 
                     if (firstDigit != 1) {
-                        num += (10L - firstDigit) * powerOfTen(digits - 1L) - 10L;
+                        num += ((10L - firstDigit) * powerOfTen(digits - 1L)) - 10L;
                         continue;
                     }
 
-                    lastDigit = (num - (10L * (num / 10L)));
-                    value = (num / 10L) + lastDigit * powerOfTen(digits - 1L);
+                    lastDigit = (num - (10L * (num/10L)));
+                    value = (num/10L) + lastDigit * powerOfTen(digits - 1L);
 
-                    if (value == num * 9L) {
-                        System.out.println("Found! -> Thread " + id + " " + result + ": " + num + " - " + value);
-                        System.out.println("Check: " + num * 9L + " - " + value / 9L);
-                        if (result == -1 || num < result)
-                            result = num;
+                    difference = Math.abs((factor * num) - value);
+
+                    if (difference < 10L) {
+                        String found = "Found! -> Thread " + id + ": " + num + " - " + value + ". Diff: " + difference;
+                        saveResult(found);
+                        System.out.println(found);
+                        System.out.println("Check: " + num * factor + " - "
+                                + (value/factor) + " Diff: " + difference);
+
+                        if (difference == 0)
+                            return;
                     }
                 }
 
@@ -96,13 +112,14 @@ public class Riddle2 {
         }
 
         private static long powerOfTen(long digits) {
-            long power = 1;
+            long power = 1L;
             for (int i = 0; i < digits; i++) {
-                power *= 10;
+                power *= 10L;
             }
-
             return power;
         }
+
     }
+
 }
 
